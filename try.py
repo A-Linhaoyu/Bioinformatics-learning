6e55fcf00192ac66b6bea1dfdad8f197ec7d626d
A-Linhaoyu <alingghhyj@gmail.com>
2026-09-14 00:14:28 +0800

    首次尝试用VS跑py

---
+++ try.py
@@ -0,0 +1,7 @@
+sequence = "ATGCGATCGATCGATCG"
+
+print("DNA sequence:", sequence)
+print("A:", sequence.count("A"))
+print("T:", sequence.count("T"))
+print("G:", sequence.count("G"))
+print("C:", sequence.count("C"))
